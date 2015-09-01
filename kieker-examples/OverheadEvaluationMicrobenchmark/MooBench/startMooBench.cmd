bad35f8d89718bf8772d6250b8a23ffb0a79b320
Christian Wulf <chw@informatik.uni-kiel.de>
2015-09-01 17:02:17 +0200

    added startMooBench

---
+++ kieker-examples/OverheadEvaluationMicrobenchmark/MooBench/startMooBench.cmd
@@ -0,0 +1,34 @@
+set JAVABIN=""
+
+set JAVAARGS=-server ^
+	-d64 ^
+	-Xms1G -Xmx4G
+
+set WEAVER_JAR=./lib/kieker-1.12-SNAPSHOT-aspectj.jar
+
+set JAVAARGS_LTW="%JAVAARGS% -javaagent:%WEAVER_JAR% ^
+	-Dorg.aspectj.weaver.showWeaveInfo=false ^
+	-Daj.weaving.verbose=false ^
+	-Dkieker.monitoring.skipDefaultAOPConfiguration=true ^
+	-Dorg.aspectj.weaver.loadtime.configuration=META-INF/kieker.aop.xml"
+
+set JAVAARGS_KIEKER_LOGGING_TCP="%JAVAARGS_LTW% ^
+	-Dkieker.monitoring.writer=kieker.monitoring.writer.tcp.TCPWriter"
+
+set JAR="-jar MooBench.jar -a mooBench.monitoredApplication.MonitoredClassSimple"
+
+set TOTALCALLS=2000000      		rem ## 2000000
+set METHODTIME=0       				rem ## 500000
+set THREADS=1               		rem ## 1
+set RECURSIONDEPTH=10       		rem ## 10
+set OUTPUT_FILENAME="%TOTALCALLS%-%METHODTIME%-%THREADS%-%RECURSIONDEPTH%"
+set MOREPARAMS="--quickstart ^
+	-r kieker.Logger"
+
+%JAVABIN%java  %JAVAARGS_KIEKER_LOGGING_TCP% %JAR% ^
+	--totalcalls %TOTALCALLS% ^
+	--methodtime %METHODTIME% ^
+	--totalthreads %THREADS% ^
+	--recursiondepth %RECURSIONDEPTH% ^
+	--output-filename %OUTPUT_FILENAME%.csv ^
+	%MOREPARAMS%
